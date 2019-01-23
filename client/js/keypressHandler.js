aef5795e911f78c1e37c05b8d78089c04eca352b
Jake Ascher <ascherj@gmail.com>
2019-01-22 19:49:16 -0500

    Add AJAX request from client to server and respond with random direction

--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -1,4 +1,3 @@
-
 $('body').on('keydown', (event) => {
   var arrowPress = event.key.match(/Arrow(Up|Down|Left|Right)/);
   if (arrowPress) {
@@ -8,10 +7,19 @@ $('body').on('keydown', (event) => {
 });
 
 $('button').on('click', (event) => {
-  var directions = ['up', 'down', 'left', 'right'];
-  var randomIndex = Math.floor(Math.random() * directions.length);
-  var randomDirection = directions[randomIndex];
-  SwimTeam.move(randomDirection);
+  // var directions = ['up', 'down', 'left', 'right'];
+  // var randomIndex = Math.floor(Math.random() * directions.length);
+  // var randomDirection = directions[randomIndex];
+  // SwimTeam.move(randomDirection);
+
+  $.ajax({
+    url: 'http://127.0.0.1:3000',
+    type: 'GET',
+    success: (data) => console.log('server responded with', data),
+    error: () => console.log('error')
+  });
 });
+
+
 
 console.log('Client is running in the browser!');
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,6 +1,30 @@
 const headers = require('./cors');
+const messageQueue = require('./messageQueue');
+
+
+var sendResponse = function(res, data, statusCode = 200) {
+  res.writeHead(statusCode, headers);
+  res.end(JSON.stringify(data));
+}
+
+var actions = {
+  'GET': function(req, res) {
+    var directions = ['up', 'down', 'left', 'right'];
+    var randomIndex = Math.floor(Math.random() * directions.length);
+    var randomDirection = directions[randomIndex];
+    sendResponse(res, randomDirection, 200);
+  },
+  'POST': function(req, res) {
+
+  }
+}
 
 module.exports = (req, res) => {
-  res.writeHead(200, headers);
-  res.end();
+  var action = actions[req.method];
+
+  if (action) {
+    action(req, res);
+  } else {
+    sendResponse(res, '', 404);
+  }
 };
